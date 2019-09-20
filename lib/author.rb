84c423efc7adcaf019a2180e3fe0e3b2c5774898
Damini Varu <damvaru4@gmail.com>
2019-09-20 16:51:15 +0000

    Done.

---
+++ lib/author.rb
@@ -0,0 +1,37 @@
+require 'pry'
+
+class Author 
+  attr_accessor :name, :posts 
+  
+  @post_count = 0
+  
+  
+  def initialize(name)
+    @name = name
+    @posts = []
+  end 
+  
+   
+  def add_post(post)
+     @post << post
+     post.author = self
+    @@post_count += 1
+  end
+  
+  def add_post_by_title(title)
+    title = Post.new(title)
+    title.author = self
+    @post << title
+    @@post_count += 1 
+  end 
+    
+  def self.post_count
+    @@post_count
+  end
+    
+    
+end 
+
+
+
+
